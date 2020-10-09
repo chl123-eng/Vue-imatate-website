80dd9a35398bd78740aaf9a500253c188bd7669c
chl <123@qq.com>
2020-10-10 00:00:29 +0800

    first commit

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" :src="bannerImg"/>
+            <div class="banner-info">
+                <div class="banner-title">
+                    {{this.sightName}}
+                </div>
+                <div class="banner-number">
+                    <span class="iconfont">&#xe635;</span>
+                    {{this.bannerImgs.length}}
+                </div>
+            </div>
+        </div>
+        <fade-animation>
+          <common-gallary
+          :imgs="bannerImgs"
+          v-show="showGallary"
+          @close="handleGallaryClose">
+          </common-gallary>
+        </fade-animation>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/gallary'
+import fadeAnimation from 'common/fade/fadeAnimation'
+export default {
+  name: 'DetailBaner',
+  props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
+  },
+  data () {
+    return {
+      showGallary: false
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary,
+    fadeAnimation
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      color: #fff
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .42rem
+        padding: 0 .4rem
+      .banner-number
+        padding: 0 .4rem
+        line-height: .4rem
+        height: .32rem
+        margin-top: .14rem
+        font-size: .24rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        .banner-iconfont
+          font-sie: .24rem
+</style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <div class="title">热销推荐</div>
+      <ul>
+        <router-link tag="li"
+          :to="'/detail/' + item.id"
+          class="item border-bottom" v-for="item of list" :key="item.id">
+          <img :src="item.imgUrl" class="item-img">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </router-link>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommed',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixnis.styl";
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 2.9rem
+    .item-img
+      width: 3.7rem
+      height: 2.7rem
+      padding: .1rem
+    .item-info
+      min-width: 0
+      flex: 1
+      padding: .1rem
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height  .44rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+        line-height: .44rem
+
+</style>
